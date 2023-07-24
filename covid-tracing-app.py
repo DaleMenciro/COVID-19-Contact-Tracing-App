c14a2880b94d376797a939b5c7ea2c645ffe9f62
Dale Menciro <dale_menciro@protonmail.com>
2023-07-24 16:23:46 +0800

    program guidelines

---
+++ covid-tracing-app.py
@@ -0,0 +1,9 @@
+'''
+This program will create covid contact tracing app with GUI
+
+Parameters:
+    - Create a program that ask user for typical information found in covid contact tracing app
+	- Write the collected information in a file (use any format you like)
+	- The app should be able to add and search entry
+	- Be creative, the realistic the better.
+'''
