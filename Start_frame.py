52666a7bb3348b3ea25817b1bd3ea7c17c326c21
Dale Menciro <dale_menciro@protonmail.com>
2023-07-27 17:04:15 +0800

    initializing widgets

--- Start_frame.py
+++ Start_frame.py
@@ -9,6 +9,12 @@ class Frame(ctk.CTkFrame):
         self.frame.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 
     def create_widget(self):
+        #Labels
+        self.label2=ctk.CTkLabel(master=self.frame, text="Welcome to",font=('Century Gothic',30, "bold"))
+        self.label2.place(x=70, y=40)
+
+        self.label3=ctk.CTkLabel(master=self.frame, text="COVID-19 Contact Tracing App",font=('Century Gothic',17))
+        self.label3.place(x=35, y=80)
 
     def buttons(self):
     
