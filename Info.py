ffc0c49d548d0042db10b98b2787f9f315c5e474
Dale Menciro <dale_menciro@protonmail.com>
2023-07-27 17:09:28 +0800

    defining functions

--- Info.py
+++ Info.py
@@ -6,3 +6,10 @@ class Info(ctk.CTkFrame):
         super().__init__(parent)
         self.frame=ctk.CTkFrame(parent, width=700, height=550)
         self.frame.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+
+    def display_text(self):
+    
+    def create_button(self):
+    
+    def button_command(self):
+
