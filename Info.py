e1f779aaae38ec66662338fa7205355286fcc519
Dale Menciro <dale_menciro@protonmail.com>
2023-07-27 17:08:59 +0800

    initializing class and importing needed module

---
+++ Info.py
@@ -0,0 +1,8 @@
+import tkinter
+import customtkinter as ctk
+
+class Info(ctk.CTkFrame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.frame=ctk.CTkFrame(parent, width=700, height=550)
+        self.frame.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
