8d96d4c940e24db0f913018532b23593118cc797
Dale Menciro <dale_menciro@protonmail.com>
2023-07-28 09:45:46 +0800

    initializing COVID class

--- COVID.py
+++ COVID.py
@@ -3,4 +3,9 @@ import tkinter
 from tkinter import messagebox
 
 class COVID(ctk.CTkFrame):
-    
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.frame=ctk.CTkFrame(parent, width=900, height=650)
+        self.frame.place(relx=0.5, rely=0.45, anchor=tkinter.CENTER)
+        self.frame2=ctk.CTkFrame(parent, width=900, height=70)
+        self.frame2.place(relx=0.5, rely=0.94, anchor=tkinter.CENTER)
