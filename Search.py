33cb1e975fc50eff5a4fabcc0ffdb173fe6010af
Dale Menciro <dale_menciro@protonmail.com>
2023-07-29 01:06:00 +0800

    copying parameters from COVID on widgets

--- Search.py
+++ Search.py
@@ -12,5 +12,44 @@ class Search(ctk.CTkFrame):
         self.frame2.place(relx=0.5, rely=0.94, anchor=tkinter.CENTER)    
 
     def create_widgets(self):
+         #respondent information labels
+        self.title_label = ctk.CTkLabel(master=self.frame,bg_color="#367E18", width=900, height=40, text = "Respondent Information",text_color="#FCFFE7", font=("Century Gothic", 25, "bold"))
+        self.title_label.place(relx=0.5, rely=0.05, anchor=tkinter.CENTER)
+        
+        #name label
+        self.name_label = ctk.CTkLabel(master=self.frame, text = "Name:", font=("Century Gothic", 17, "bold"))
+        self.name_label.place(x= 50, y=80)
+
+        #gender label
+        self.gender_label = ctk.CTkLabel(master=self.frame, text = "Gender:", font=("Century Gothic", 17, "bold")) 
+        self.gender_label.place(x= 50, y=130)
+
+        #number label
+        self.number_label = ctk.CTkLabel(master= self.frame, text = "Phone Number:" , font=("Century Gothic", 17, "bold"))
+        self.number_label.place(x= 50, y=180)
+
+        #email label
+        self.email_label = ctk.CTkLabel(master= self.frame, text = "Email Address:", font=("Century Gothic", 17, "bold"))
+        self.email_label.place(x= 50, y=230)
+
+        #health questions labels
+        self.title2_label = ctk.CTkLabel(master=self.frame,bg_color="#367E18", width=900, height=40, text = "Health Questions",text_color="#FCFFE7", font=("Century Gothic", 25, "bold"))
+        self.title2_label.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+
+        #vaccination_status label
+        self.vaccination_status_label = ctk.CTkLabel(master=self.frame, text = "Have you received the COVID-19 vaccination?", font=("Century Gothic", 17, "bold"))
+        self.vaccination_status_label.place(x= 50, y=370)
+
+        #symptoms label
+        self.symptoms_label = ctk.CTkLabel(master=self.frame, text = "Have you been exposed to COVID-19 in the last 14 days?",font=("Century Gothic", 17, "bold"))
+        self.symptoms_label.place(x= 50, y=440)
+
+        #exposure label
+        self.exposure_label = ctk.CTkLabel(master=self.frame, text = "Have you been in contact with someone experiencing COVID-19 symptoms in the past week?",font=("Century Gothic", 17, "bold"))
+        self.exposure_label.place(x= 50, y=510)
+
+        #covid_test label
+        self.covid_test_label = ctk.CTkLabel(master=self.frame, text = "Have you undergone a COVID-19 test within the past two weeks?",font=("Century Gothic", 17, "bold"))
+        self.covid_test_label.place(x= 50, y=580)
 
     def entries(self):
