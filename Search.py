1428d1346ddf67a659b18a26074f67f83711f687
Dale Menciro <dale_menciro@protonmail.com>
2023-07-29 01:36:09 +0800

    fixing title label

--- Search.py
+++ Search.py
@@ -16,10 +16,9 @@ class Search(ctk.CTkFrame):
         self.back_exit_button() 
 
     def create_widgets(self):
-         #respondent information labels
-        self.title_label = ctk.CTkLabel(master=self.frame2, text = "Enter your Registration Number:", font=("Century Gothic", 18, "bold"))
-        self.title_label.place(relx=0.2, rely=0.5, anchor=tkinter.CENTER)
-        
+        #respondent information labels
+        self.title_label = ctk.CTkLabel(master=self.frame,bg_color="#367E18", width=900, height=40, text = "Respondent Information",text_color="#FCFFE7", font=("Century Gothic", 25, "bold"))
+        self.title_label.place(relx=0.5, rely=0.05, anchor=tkinter.CENTER)
         #name label
         self.name_label = ctk.CTkLabel(master=self.frame, text = "Name:", font=("Century Gothic", 17, "bold"))
         self.name_label.place(x= 50, y=80)
