188d3b4ab5c762b0918195233e3b464f19b932fe
Dale Menciro <dale_menciro@protonmail.com>
2023-07-28 09:47:59 +0800

    name label

--- COVID.py
+++ COVID.py
@@ -15,6 +15,9 @@ class COVID(ctk.CTkFrame):
         self.title_label = ctk.CTkLabel(master=self.frame,bg_color="#367E18", width=900, height=40, text = "Respondent Information",text_color="#FCFFE7", font=("Century Gothic", 25, "bold"))
         self.title_label.place(relx=0.5, rely=0.05, anchor=tkinter.CENTER)
         
+        #name label
+        self.name_label = ctk.CTkLabel(master=self.frame, text = "Name:", font=("Century Gothic", 17, "bold"))
+        self.name_label.place(x= 50, y=80)
 
     def entries(self):
 
